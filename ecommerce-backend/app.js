72b294d89d5cf24245cdfe03089df6cab05d0da0
Jayesh Pansuriya <pansuriyajayesh786@gmail.com>
2025-07-18 11:58:05 +0530

    "creating the port and conect databases configuration "

---
+++ ecommerce-backend/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+
+//config 
+dotenv.config();//? database configuration from this config file will redirect to the app.js
+const app = express();//? whole express is stord in this config 
+
+//middleware
+app.use(express.json()); //? Parses incoming JSON data from the request body Converts it into a JavaScript object So you can easily use req.body in your routes
+app.use(cors())//? This enables CORS (Cross-Origin Resource Sharing).
+// When your frontend (like React) is hosted on one domain (e.g. localhost:3000)
+// and your backend (Node.js) is on another (e.g. localhost:5000),
+// CORS is required to allow communication between them.
+
+//test route
+app.get("/",(req,res)=>{
+    res.send("API is running..");
+});
+
+const PORT = process.env.PORT || 5000;
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    app.listen(PORT,()=> console.log(`Server running at PORT ${PORT}`));
+
+})
+.catch((err)=> console.log("MongoDB connection failed:" ,err))
