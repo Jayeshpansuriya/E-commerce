d5df0cd0821191631c36b13dd0aa9a70016a6ae9
Jayesh Pansuriya <pansuriyajayesh786@gmail.com>
2025-07-18 16:16:12 +0530

    "Auth Middleware – Protected Routes Setup JWT Protected Route pe successfully access kar raha hai — matlab authentication ka flow sahi se kaam kar raha hai 💯  "

--- ecommerce-backend/routes/userRoutes.js
+++ ecommerce-backend/routes/userRoutes.js
@@ -1,7 +1,22 @@
 import express from "express";
 import { registerUser,loginUser } from "../controllers/userController.js";
+import protect from "../middlewares/authMiddleware.js";
+
+
+
+
+
 
 const router = express.Router();
+// @route   GET /api/users/profile
+router.get("/profile",protect,(req,res)=>{
+    res.status(200).json({
+        message:"profile fetched successfully",
+        user: req.user
+    });
+});
+
+
 
 router.post("/register",registerUser);
 router.post("/login",loginUser);
