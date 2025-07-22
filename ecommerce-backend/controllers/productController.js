b81aa9517283ba5e19f16f163968785cb7fad8b9
Jayesh Pansuriya <pansuriyajayesh786@gmail.com>
2025-07-22 19:11:13 +0530

    "get product by Id "

--- ecommerce-backend/controllers/productController.js
+++ ecommerce-backend/controllers/productController.js
@@ -1,14 +1,30 @@
 import Product from "../models/productModel.js";
 //? get all product
 export const getAllProducts = async (req, res) => {
-  const products = await Product.find({});
-  res.status(200).json(products);
+  try {
+    const products = await Product.find().populate("createdBy", "name email role");
+      res.json(products);
+
+
+  } catch (error) {
+    console.error("Error fetching products:", error);
+      res.status(200).json({message: "Server error while fetching products"});
+  }
+ 
+
 };
 //? get single product by id 
 export const getProductById = async (req, res) => {
-  const product = await Product.findById(req.params.id);
-  if (!product) return res.status(404).json({ message: "product not found" });
+ try{
+  const product = await Product.findById(req.params.id).populate("createdBy","name email");
+  if(!product){
+    return res.status(404).json({message: "Product not Found"});
+  }
   res.json(product);
+ }catch(error){
+  console.error("Error fetching product:", error);
+  res.status(500).json({message: "Server error while fetching product "});
+ }
 };
 
 //? create new product(admin only)
